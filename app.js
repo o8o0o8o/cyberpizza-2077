1d21ed46f60f04538db6f26205ee89e7e5adfec4
Siarhei Labuts <sergeylabut@gmail.com>
2020-11-26 19:07:11 +0300

    fix: close connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let express = require("express");
-let app = express();
-let arr = [
+const express = require("express");
+const app = express();
+const arr = [
   { name: "Margherita", price: 5 },
   { name: "Napolitana", price: 6 },
   { name: "Capricciosa", price: 5.5 },
@@ -10,6 +10,11 @@ let arr = [
 ];
 const mongoose = require("mongoose");
 const db = mongoose.connection;
+const pizzaSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+});
+const Pizza = mongoose.model("Pizza", pizzaSchema);
 
 app.get("/", function (req, res) {
   res.send("To view pizzas send GET request '/api/products'");
@@ -22,15 +27,10 @@ app.get("/api/products", function (req, res) {
   );
   db.on("error", console.error.bind(console, "connection error:"));
   db.once("open", async function () {
-    const pizzaSchema = new mongoose.Schema({
-      name: String,
-      price: Number,
-    });
-    const Pizza = mongoose.model("Pizza", pizzaSchema);
-
     const pizzaList = arr.map(
       (a) => new Pizza({ name: a.name, price: a.price })
     );
+
     pizzaList.forEach((a) => a.save());
 
     const pizzaFromDB = await Pizza.find({});
