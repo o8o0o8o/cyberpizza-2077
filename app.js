c25b308938bdb5d221b83161bffc0991623dac29
Siarhei Labuts <sergeylabut@gmail.com>
2020-11-26 18:08:00 +0300

    feat: mongoDB and mongoose

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/", function (req, res) {
 
 app.get("/api/products", function (req, res) {
   mongoose.connect(
-    "mongodb+srv://cyberpizza-2077:cyberpizza-2077@cluster0.k7nnm.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://cyberpizza-2077:cyberpizza-2077@cluster0.k7nnm.mongodb.net/test?ssl=true&retryWrites=true&w=majority",
     { useUnifiedTopology: true, useNewUrlParser: true }
   );
   db.on("error", console.error.bind(console, "connection error:"));
