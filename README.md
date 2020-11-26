4f37bb0dbf7e2883669a134dd81cdaec334a0d81
Siarhei Labuts <sergeylabut@gmail.com>
2020-11-26 16:13:01 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# cyberpizza-2077
---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let app = express();
+let arr = [
+  { name: "Margherita", price: 5 },
+  { name: "Napolitana", price: 6 },
+  { name: "Capricciosa", price: 5.5 },
+  { name: "Mexicana", price: 7 },
+  { name: "Vegetariana", price: 8 },
+  { name: "Ciao-ciao", price: 6 },
+];
+
+app.get("/", function (req, res) {
+  res.send("To view pizzas send GET request '/api/products'");
+});
+
+app.get("/api/products", function (req, res) {
+  res.send(JSON.stringify(arr));
+});
+
+app.listen(3000);
