c1518e5f0100b6253f94529f556b0a81a7fb4185
Siarhei Labuts <sergeylabut@gmail.com>
2020-11-30 14:17:10 +0300

    feat: add handlers to routes

--- server/routes/apiProducts.js
+++ server/routes/apiProducts.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
-const pizzaController = require("../controllers/pizzaController");
+const { Pizza } = require("../models");
 
-router.get("/", pizzaController.find_Pizza_All);
-router.get("/:id", pizzaController.find_Pizza);
-router.post("/", pizzaController.find_Pizza);
-router.put("/", pizzaController.create_Pizza);
-router.delete("/", pizzaController.delete_Pizza);
+router.get("/", findPizzaAll);
+router.get("/:id", findPizzaById);
+router.post("/", findPizzaById);
+router.put("/", createPizza);
+router.delete("/", deletePizzaById);
+
+async function createPizza(req, res) {
+  const { name, price, description, weight, data, image } = req.body;
+  const pizza = await Pizza.create({
+    name,
+    price,
+    description,
+    weight,
+    data,
+    image,
+  });
+
+  return res.send(pizza);
+}
+
+async function findPizzaById(req, res) {
+  const idParameter =
+    (req.body && req.body.id) || (req.params && req.params.id);
+  const pizza = await Pizza.findById(idParameter);
+
+  return res.send(pizza);
+}
+
+async function findPizzaAll(req, res) {
+  const pizzaArray = await Pizza.find();
+
+  return res.send(pizzaArray);
+}
+
+async function deletePizzaById(req, res) {
+  const id = req.body.id || req.params.id;
+  const pizza = await Pizza.deleteOne({ id });
+
+  return res.send(pizza);
+}
 
 module.exports = router;
